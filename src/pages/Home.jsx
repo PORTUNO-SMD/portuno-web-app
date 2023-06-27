a7dd4060d4c352b2c05e19a006583bbfe4c54c0e
João Victor Alves <you@example.com>
2023-06-27 13:39:07 -0300

    Responsividade Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import Head from 'next/head'; // Importe o componente Head
 import Style from "../styles/Home.module.css";
 import Room from '../components/room';
-import { ST } from 'next/dist/shared/lib/utils';
 
 const Home = () => {
     const router = useRouter();
@@ -24,6 +23,7 @@ const Home = () => {
             .then(response => response.json())
             .then(data => {
                 setData(data.data);
+                setClassrooms(data.data.filter(classroom => classroom.floor == floor))
             })
             .catch(error => {
                 console.error('Error fetching classrooms:', error);
@@ -40,13 +40,53 @@ const Home = () => {
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
             </Head>
             <header className={Style.Header}>
-                <img className={Style.Logo} src="assets/logotipo.svg" alt="Portuno" width="250px"/>
+                <div className={Style.menuNav}>
+                    <img className={Style.Logo} src="assets/logotipo.svg" alt="Portuno" width="250px" />
+                    <nav>
+                        <ul className={Style.menuOpts}>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Ocupação</a></li>
+                            <li><a href="">Permissão</a></li>
+                            <li><a href="">Perfil</a></li>
+                        </ul>
+                    </nav>
+                </div>
                 <div className={Style.selectFloor}>
-                    <select className={Style.floorSelected} onChange={(e) => setFloor(e.target.value)}>
-                        <option value={0}>Térreo</option>
-                        <option value={1}>1º Andar</option>
-                        <option value={2}>2º Andar</option>
-                    </select>
+                    <div className={Style.floorSelected}>
+                        <label>
+                            <input
+                                type="radio"
+                                name="floor"
+                                value={0}
+                                checked={floor === 0}
+                                onChange={() => setFloor(0)}
+                                className={Style.radioButton}
+                            />
+                            <span className={Style.radioButtonLabel}>Térreo</span>
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="floor"
+                                value={1}
+                                checked={floor === 1}
+                                onChange={() => setFloor(1)}
+                                className={Style.radioButton}
+                            />
+                            <span className={Style.radioButtonLabel}>1º Andar</span>
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="floor"
+                                value={2}
+                                checked={floor === 2}
+                                onChange={() => setFloor(2)}
+                                className={Style.radioButton}
+                            />
+                            <span className={Style.radioButtonLabel}>2º Andar</span>
+                        </label>
+                    </div>
                 </div>
             </header>
             <div className={Style.Container}>
@@ -54,7 +94,6 @@ const Home = () => {
                     <Room key={classroom.id} classroom={classroom} />
                 ))}
             </div>
-
         </div>
     );
 };
